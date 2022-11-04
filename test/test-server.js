2d94c9c48a3131ad3ea9f1f00034f4bb3e8c57e4
Parker Addison <99682978+paddison-d@users.noreply.github.com>
2022-11-04 19:07:57 +0000

    Fix npm test to pass

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to For The Seven Ronin's application</h1>"));
         return done();
       });
 
